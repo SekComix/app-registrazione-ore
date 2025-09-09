5df598d8b02e3b98d1b43ce3d7f6726e5816e6f4
SekComix <cominellis57@gmail.com>
2025-09-09 13:47:33 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'report-interventi-cache-v79'; // Aumentiamo la versione per sicurezza
+const CACHE_NAME = 'report-interventi-cache-v80'; // Aumentiamo la versione per sicurezza
 const urlsToCache = [
     '/',
     '/index.html',
